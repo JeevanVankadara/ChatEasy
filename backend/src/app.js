b47e7bba7661eb8b9ccdce7867fe69710dbe0d85
JeevanVankadara <jeevanv1997@gmail.com>
2025-12-31 17:49:58 +0530

    dfggf

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.NODE_ENV === "production" ? true : 'http://localhost:5173',
   credentials: true,
 }));
 
@@ -28,7 +28,7 @@ app.use('/api/message', messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.use((req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 } else {
--- backend/src/lib/generateToken.js
+++ backend/src/lib/generateToken.js
@@ -9,5 +9,6 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, 
     httpOnly: true, 
     sameSite: "strict", 
+    secure: process.env.NODE_ENV === "production",
   });
 }
