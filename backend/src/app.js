ba418d3103e1d065f4a0cdb5f6130bdc9f21f9c9
JeevanVankadara <jeevanv1997@gmail.com>
2025-12-31 11:39:29 +0530

    deployment changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,18 +10,21 @@ import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 import messageRoutes from './routes/message.route.js';
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5001;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true,
 }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 io.on("connection", (socket) => {
   console.log('a user is connected ', socket.id);
