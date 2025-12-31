25785e623a69fc75ad592d1cf4a2be481a860fcb
JeevanVankadara <jeevanv1997@gmail.com>
2025-12-31 16:31:02 +0530

    last chance

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
 import {app, server, io, usersMap} from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
@@ -11,12 +12,13 @@ import { connectDB } from './lib/db.js';
 import messageRoutes from './routes/message.route.js';
 
 const port = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 
@@ -43,6 +45,14 @@ io.on("connection", (socket) => {
     }
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
