7118745d6113bde3e05543e09ad39e227626be53
JeevanVankadara <jeevanv1997@gmail.com>
2025-12-31 11:21:40 +0530

    deployment changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const url = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: url,
   withCredentials: true,
 });
-
